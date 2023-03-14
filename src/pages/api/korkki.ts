ab44f71cab27fc394521d9cd71e432fb394a71ef
JEFFe <jeffe@jeffe.co>
2023-03-14 22:30:19 +0200

    fix: exploit script injection stuff

--- src/pages/api/korkki.ts
+++ src/pages/api/korkki.ts
@@ -1,6 +1,6 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
-import * as cheerio from 'cheerio';
-import {WpPost} from "@shl/wptypes";
+import type { NextApiRequest, NextApiResponse } from "next";
+import * as cheerio from "cheerio";
+import { WpPost } from "@shl/wptypes";
 
 export interface Article {
   title: string;
@@ -12,25 +12,28 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Article>
 ) {
-  if (!req.query.url) throw new Error('No url')
+  if (!req.query.url) throw new Error("No url");
 
-  const resp = await fetch(req.query.url as string)
-  const html = await resp.text()
+  if (!(req.query.url as string).startsWith("https://shl.fi/"))
+    throw new Error("No href");
 
-  const $ = cheerio.load(html)
+  const resp = await fetch(req.query.url as string);
+  const html = await resp.text();
 
-  const link = $(`link[type='application/json']`).attr()
-  const href = link?.href
+  const $ = cheerio.load(html);
 
-  if (!href) throw new Error('No href')
-  const id = href.split('posts/').pop()
+  const link = $(`link[type='application/json']`).attr();
+  const href = link?.href;
 
-  const apiResp = await fetch(`https://shl.fi/wp-json/wp/v2/posts/${id}`)
-  const json = await apiResp.json() as WpPost
+  if (!href) throw new Error("No href");
+  const id = href.split("posts/").pop();
+
+  const apiResp = await fetch(`https://shl.fi/wp-json/wp/v2/posts/${id}`);
+  const json = (await apiResp.json()) as WpPost;
 
   res.status(200).json({
     post: json.content.rendered,
     link: json.link,
-    title: json.title.rendered
-  })
+    title: json.title.rendered,
+  });
 }
